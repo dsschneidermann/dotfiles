faa6d1c904b650c56b7eab1c5a622262f586ae33
Dennis Schneidermann <dsschneidermann@gmail.com>
2025-03-11 19:18:33 +0000

    Support devcontainers in gitpod

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 # Link dotfiles
-ln -s .dotfiles/.bash_aliases .bash_aliases
+declare dotfilesDir=dotfiles
+[ -d .dotfiles ] && dotfilesDir=.dotfiles
 
-cp -f .dotfiles/.gitconfig .gitconfig
+if [ -d "${dotfilesDir}" ]; then
+
+ln -sf "${dotfilesDir}"/.bash_aliases .bash_aliases
+
+# Get default git settings.
+GIT_USER_EMAIL=$(git config user.email)
+GIT_USER_NAME=$(git config user.name)
+GIT_CRED_HELPER=$(git config credential.helper)
+
+cp -f "${dotfilesDir}"/.gitconfig .gitconfig
 
 # Reset to default git configs.
-git config --global --unset credential.helper
-git config --global credential.helper "/usr/bin/gp credential-helper"
-git config --global user.email "${GITPOD_GIT_USER_EMAIL}"
-git config --global user.name "${GITPOD_GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}"
+git config --global user.name "${GIT_USER_NAME}"
+git config --global credential.helper "${GIT_CRED_HELPER}"
 git config --global init.defaultBranch main
 git config --global pull.ff only
+
+fi
